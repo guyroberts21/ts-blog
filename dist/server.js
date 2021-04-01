0c39d923a3a058ddb0ff13c03bf2072370b68a8a
Guy Roberts <guyroberts21@outlook.com>
2021-04-01 22:24:22 +0100

    Add post routing

--- dist/server.js
+++ dist/server.js
@@ -10,13 +10,19 @@ var showdown_1 = __importDefault(require("showdown"));
 var app = express_1.default();
 var converter = new showdown_1.default.Converter();
 converter.setOption('noHeaderId', true);
-var testMessage = fs_1.default.readFileSync(path_1.default.join(__dirname, '../posts/test.md'), 'utf-8');
-var convertedMsg = converter.makeHtml(testMessage);
-console.log(convertedMsg);
+// const testMessage = fs.readFileSync(
+//   path.join(__dirname, '../posts/test.md'),
+//   'utf-8'
+// );
+// const convertedMsg = converter.makeHtml(testMessage);
+// console.log(convertedMsg);
 app.get('/', function (req, res) {
     res.sendFile('index.html', { root: path_1.default.join(__dirname, '../views') });
 });
-app.get('/post', function (req, res) {
-    res.send(convertedMsg);
+app.get('/:postId', function (req, res) {
+    var postId = req.params.postId;
+    var postMessageData = fs_1.default.readFileSync(path_1.default.join(__dirname, "../posts/" + postId + ".md"), 'utf-8');
+    var postMessageHTML = converter.makeHtml(postMessageData);
+    res.send(postMessageHTML);
 });
 app.listen(8090);
--- src/server.ts
+++ src/server.ts
@@ -7,19 +7,25 @@ const app = express();
 const converter = new showdown.Converter();
 converter.setOption('noHeaderId', true);
 
-const testMessage = fs.readFileSync(
-  path.join(__dirname, '../posts/test.md'),
-  'utf-8'
-);
-const convertedMsg = converter.makeHtml(testMessage);
-console.log(convertedMsg);
+// const testMessage = fs.readFileSync(
+//   path.join(__dirname, '../posts/test.md'),
+//   'utf-8'
+// );
+// const convertedMsg = converter.makeHtml(testMessage);
+// console.log(convertedMsg);
 
 app.get('/', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../views') });
 });
 
-app.get('/post', (req, res) => {
-  res.send(convertedMsg);
+app.get('/:postId', (req, res) => {
+  const { postId } = req.params;
+  const postMessageData = fs.readFileSync(
+    path.join(__dirname, `../posts/${postId}.md`),
+    'utf-8'
+  );
+  const postMessageHTML = converter.makeHtml(postMessageData);
+  res.send(postMessageHTML);
 });
 
 app.listen(8090);
